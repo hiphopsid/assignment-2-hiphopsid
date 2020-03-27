403f5f757397bed54497fcf8193042390d5d4708
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2020-03-27 19:24:11 +0530

    Update Student.java

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -8,5 +8,39 @@ package problem5.student;
 // to store student information and properties
 public class Student 
 {
-  
+      private int rollNumber;
+    private String Name;
+    private int BackLogCounter;
+    private int appearingCounter;
+
+    public Student(int rollNumber,String name)
+    {
+        this.rollNumber=rollNumber;
+        Name=name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return Name;
+    }
+    
+
+    public int getAppearingCounter() {
+        return appearingCounter;
+    }
+
+    public void setAppearingCounter(int appearingCounter) {
+        this.appearingCounter = appearingCounter;
+    }
+
+    public void setBackLogCounter(int backLogCounter) {
+        BackLogCounter = backLogCounter;
+    }
 }

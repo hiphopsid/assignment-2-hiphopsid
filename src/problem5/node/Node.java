82cbd7902c85d34e7c2972581bf7d84019c9b493
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2020-03-27 19:06:26 +0530

    Update Node.java

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -6,5 +6,29 @@
  */
 package problem5.node;
 // to define node properties
-public class Node {
+public class Node 
+{
+   private Student student;
+    private Node next;
+    public Node(Student student)
+    {
+        this.student=student;
+        next=null;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }

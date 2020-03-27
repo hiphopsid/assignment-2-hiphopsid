0503c64940600a92c07150d0f12a9324dc52ffa5
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2020-03-27 20:42:55 +0530

    Update MyMain.java

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,5 +7,20 @@
 package problem4.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
-public class MyMain {
+public class MyMain 
+{
+      public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(13);
+        obj.insert(33);
+        obj.insert(30);
+        obj.insert(32);
+        obj.insert(10);
+        obj.insert(11);
+        obj.insert(27);
+
+        MyQueue mq = new MyQueue();
+        mq.preSuccessor(obj.getroot());
+        mq.getPreSucc();
+    }
 }
